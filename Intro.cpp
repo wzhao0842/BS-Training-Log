26fd64ffcd9b8309c59316c47bc7932b51c431fc
Sunny <73135987+wzhao0842@users.noreply.github.com>
2021-10-23 18:10:14 -0700

    Update Intro.cpp

--- Intro.cpp
+++ Intro.cpp
@@ -27,3 +27,45 @@ int main(){
         }
     }
 }
+
+//fast searching 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    int ar[n]; for(int i=0;i<n;i++) cin >> ar[i];
+    sort(ar, ar+n);
+    int m; cin >> m;
+    for(int i=0;i<m;i++){
+        int a, b; cin >> a >> b;
+        int left = -1, right = -1;
+        int l = -1, r = n;
+        //lower bound
+        while(l<r-1){
+            int mid = (l+r)/2;
+            if(ar[mid] >= a){
+                r = mid;
+            }else{
+                l = mid;
+            }
+        }
+        if(ar[r] >= a) left = r; 
+        l = -1, r = n;
+        while(l<r-1){
+            int mid = (l+r)/2;
+            if(ar[mid] <= b){
+                l = mid;
+            }else{
+                r = mid;
+            }
+        }
+        if(ar[l] <= b) right = l;
+        if(left != -1 && right != -1){
+            cout << right - left + 1 << " ";
+        }else{
+            cout << 0 << " ";
+        }
+    }
+}
